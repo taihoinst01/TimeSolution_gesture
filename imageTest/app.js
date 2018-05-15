ff9592cf14ef27642a599b5769aeb295dfc7dd05
Sinook Kang <sokang337@gmail.com>
2018-05-15 17:05:52 +0900

    update

--- imageTest/app.js
+++ imageTest/app.js
@@ -14,8 +14,10 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
+//app.set('view engine', 'ejs');
+//app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.engine('html', require('html').renderFile);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
